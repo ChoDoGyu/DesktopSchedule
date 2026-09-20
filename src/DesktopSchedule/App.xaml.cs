23b55524f816a2d7edf65024a3f7b7ea75fa93ca
ChoDoGyu <doing9498@gmail.com>
2026-09-21 02:40:59 +0900

    refactor: 초기 WPF 코드 정리

--- src/DesktopSchedule/App.xaml.cs
+++ src/DesktopSchedule/App.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace DesktopSchedule;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
 }
-
